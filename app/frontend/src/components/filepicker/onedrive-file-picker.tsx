0ea1ef84d2d18ab99a3fd7edc53490ea8568f008
Nilesh Verma <nilesh.verma@ais.com>
2025-02-05 13:14:52 +0000

    Updated the body sent by file-picker & onefrive-file-picker

--- app/frontend/src/components/filepicker/onedrive-file-picker.tsx
+++ app/frontend/src/components/filepicker/onedrive-file-picker.tsx
@@ -357,6 +357,7 @@ const OneDriveFilePicker = ({ folderPath, tags }: Props) => {
           // Append file and other data to FormData  
           data.append("file", file);
           data.append("file_path", filePath);
+          data.append("uploadSource", "OneDrive");
 
           if (tags.length > 0) {
             data.append("tags", tags.map(encodeURIComponent).join(","));
